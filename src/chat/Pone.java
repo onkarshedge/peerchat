04edc511575980923236fd1191a878583b8e1c09
onkarshedge <shedge31onkar@gmail.com>
2015-10-02 16:46:23 +0530

    added multicast for online users total 3 threads

--- src/chat/Pone.java
+++ src/chat/Pone.java
@@ -1,13 +1,51 @@
 package chat;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
+import java.util.HashSet;
+import java.util.Set;
+
+class User {
+	final String Username;
+	final InetSocketAddress location;
+ public	User(String u,InetSocketAddress ad){
+		Username=u;
+		location=ad;
+	}
+ @Override
+ public boolean equals(Object other){
+	 if(other == null || !(other instanceof User))
+		 return false;
+	 if(other == this) 
+		 return true;
+	 User rhs=(User) other;
+	 if(Username.equals(rhs.Username) && location.equals(rhs.location))
+		 return true;
+	 return false;
+	 
+ }
+ @Override
+ public int hashCode(){
+	 int hash=3;
+	 hash=hash*7+Username.hashCode();
+	 hash=hash*7+location.hashCode();
+	 return hash;
+ }
+ @Override
+ public String toString(){
+	 return Username+" "+location.toString();
+ }
+	
+}
 
 public class Pone {
 	Thread t1=new Thread(){
 		public void run(){
+			byte [] buf=new byte[1024];
+			DatagramPacket pk_recv=new DatagramPacket(buf,buf.length);
 			while(true){
 				try {
 					soc.receive(pk_recv);
@@ -15,40 +53,106 @@ public class Pone {
 					e.printStackTrace();
 				}
 				String msg=new String(pk_recv.getData(),0,pk_recv.getLength());
-				System.out.println(pk_recv.getSocketAddress()+msg);
+				System.out.println(pk_recv.getSocketAddress().toString()+" "+msg);
 			}
 		}
 	};
+	Thread t2=new Thread(){
+		public void run(){
+			InetAddress adrs=null;
+			try {
+				adrs=InetAddress.getByName(Multicast_ip_address);
+			} catch (UnknownHostException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			while(true){
+				String msg=Username;
+				DatagramPacket pck=new DatagramPacket(msg.getBytes(), msg.length(),adrs,MPORT);
+				try {
+					soc.send(pck);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				try {
+					Thread.sleep(3500);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	};
+	Thread t3=new Thread(){
+		 public void run(){
+			byte[] msg=new byte[1024];
+			MulticastSocket recv_broadcast = null;
+			try {
+				recv_broadcast = new MulticastSocket(MPORT);
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			try {
+				recv_broadcast.joinGroup(InetAddress.getByName(Multicast_ip_address));
+			} catch (UnknownHostException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			while(true){
+			try {
+				DatagramPacket msgpackt=new DatagramPacket(msg, msg.length);
+				recv_broadcast.receive(msgpackt);
+				String frnd_name=new String(msgpackt.getData(),0,msgpackt.getLength());
+				SocketAddress frnd_adrs=msgpackt.getSocketAddress();
+				User e=new User(frnd_name,(InetSocketAddress)frnd_adrs);
+				friends.add(e);
+				for(User p:friends){
+					System.out.println(p.toString());
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+				//recv_broadcast.leaveGroup(InetAddress.getByName(inet_adr));
+				recv_broadcast.close();
+			}
+		  }
+		}
+	};
 	
-	byte[] buf;
+	
 	DatagramSocket soc;
-	DatagramPacket pk_recv,pk_send;
+	final int MPORT=12002;
+	final String Multicast_ip_address="228.0.0.3";
+	final String Username="ramesh";
+	Set<User> friends=new HashSet<User>();
 	public Pone() {
-		byte[] buf=new byte[1024];
 		try {
-			soc=new DatagramSocket(55210);
+			soc=new DatagramSocket();
 		} catch (SocketException e) {
 			e.printStackTrace();
 		}
-		pk_send=new DatagramPacket(buf,buf.length,new InetSocketAddress(30001));
-		pk_recv=new DatagramPacket(buf, buf.length);
+
 	}
 	public void send(String messg){
-		pk_send.setData(messg.getBytes());
+		DatagramPacket pk_send=new DatagramPacket(messg.getBytes(),messg.getBytes().length);
 		try {
 			soc.send(pk_send);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	void recv() {
+	void init() {
+		t2.start();
+		t3.start();
 		t1.start();
+		
 	}
 	
 	
 	public static void main(String[] args) {
 		Pone obj =new Pone();
-		obj.recv();
+		obj.init();
 		
 		while(true){
 		try {
--- src/chat/Ptwo.java
+++ src/chat/Ptwo.java
@@ -1,66 +1,134 @@
 package chat;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
+import java.util.HashSet;
+import java.util.Set;
+
+
 
 public class Ptwo {
 	Thread t1=new Thread(){
 		public void run(){
+			byte [] buf=new byte[1024];
+			DatagramPacket pk_recv=new DatagramPacket(buf,buf.length);
 			while(true){
 				try {
 					soc.receive(pk_recv);
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 				String msg=new String(pk_recv.getData(),0,pk_recv.getLength());
-				System.out.println(msg);
+				System.out.println(pk_recv.getSocketAddress().toString()+" "+msg);
 			}
 		}
 	};
+	Thread t2=new Thread(){
+		public void run(){
+			InetAddress adrs=null;
+			try {
+				adrs=InetAddress.getByName(Multicast_ip_address);
+			} catch (UnknownHostException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			while(true){
+				String msg=Username;
+				DatagramPacket pck=new DatagramPacket(msg.getBytes(), msg.length(),adrs,MPORT);
+				try {
+					soc.send(pck);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				try {
+					Thread.sleep(3500);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	};
+	Thread t3=new Thread(){
+		 public void run(){
+			byte[] msg=new byte[1024];
+			MulticastSocket recv_broadcast = null;
+			try {
+				recv_broadcast = new MulticastSocket(MPORT);
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			try {
+				recv_broadcast.joinGroup(InetAddress.getByName(Multicast_ip_address));
+			} catch (UnknownHostException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			while(true){
+			try {
+				DatagramPacket msgpackt=new DatagramPacket(msg, msg.length);
+				recv_broadcast.receive(msgpackt);
+				String frnd_name=new String(msgpackt.getData(),0,msgpackt.getLength());
+				SocketAddress frnd_adrs=msgpackt.getSocketAddress();
+				User e=new User(frnd_name,(InetSocketAddress)frnd_adrs);
+				friends.add(e);
+				for(User p:friends){
+					System.out.println(p.toString());
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+				//recv_broadcast.leaveGroup(InetAddress.getByName(inet_adr));
+				recv_broadcast.close();
+			}
+		  }
+		}
+	};
 	
-	byte[] buf;
+	
 	DatagramSocket soc;
-	DatagramPacket pk_recv,pk_send;
+	final int MPORT=12002;
+	final String Multicast_ip_address="228.0.0.3";
+	final String Username="suresh";
+	Set<User> friends=new HashSet<User>();
 	public Ptwo() {
-		byte[] buf=new byte[1024];
 		try {
-			soc=new DatagramSocket(30001);
+			soc=new DatagramSocket();
 		} catch (SocketException e) {
 			e.printStackTrace();
 		}
-		pk_send=new DatagramPacket(buf,buf.length,new InetSocketAddress(55210));
-		pk_recv=new DatagramPacket(buf, buf.length);
+
 	}
 	public void send(String messg){
-		pk_send.setData(messg.getBytes());
+		DatagramPacket pk_send=new DatagramPacket(messg.getBytes(),messg.getBytes().length);
 		try {
 			soc.send(pk_send);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	void recv() {
+	void init() {
+		t3.start();
 		t1.start();
+		t2.start();
 	}
 	
 	
 	public static void main(String[] args) {
-		Ptwo obj =new Ptwo();
-		obj.recv();
+		Pone obj =new Pone();
+		obj.init();
 		
 		while(true){
 		try {
 			obj.send(new BufferedReader(new InputStreamReader(System.in)).readLine());
 		} catch (IOException e) {
-
 			e.printStackTrace();
 		}
 		}
 	}
 
 }
-
